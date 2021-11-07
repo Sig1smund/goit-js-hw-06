85a5ae064498a1209d6cc56bb91a9c0dee71e34a
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2021-11-07 16:28:31 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const mainList = document.querySelector('#ingredients');
+
+const listItems = ingredients.map(item => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('.item');
+  listItem.textContent = item;
+
+  return listItem;
+});
+
+mainList.append(...listItems);
+  
+
+
