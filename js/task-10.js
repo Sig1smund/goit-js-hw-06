9e18044ddb57ff7c546342850ef4829fea6be706
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2021-11-09 23:49:17 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const container = document.getElementById('boxes');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const amountSetter = document.querySelector('input');
+
+const nums = [];
+
+
+
+const countDivs = (event) => {
+  nums.push(event.currentTarget.value);
+
+  return nums;
+}
+
+amountSetter.addEventListener('input', countDivs)
+
+createBtn.addEventListener('click', createBoxes(nums));
+// destroyBtn.addEventListener('click');
+
+
+function createBoxes(amount) {
+  let baseWidth = '10px';
+  let baseHeight = '10px'
+  nums.forEach(item => {
+    const makeDiv = document.createElement('div');
+    makeDiv.width += baseWidth;
+    makeDiv.height += baseHeight;
+    makeDiv.backgroundColor = getRandomHexColor;
+
+    return makeDiv;
+
+  });
+}
