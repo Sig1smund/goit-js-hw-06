deae5b89c23ec931871c1631f85cf4be25fc2a14
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2021-11-09 21:14:21 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', formGatherFn);
+
+function formGatherFn (event) {
+    event.preventDefault();
+
+    const formElem = event.currentTarget.elements;
+
+    const mail = formElem.email.value;
+    const password = formElem.password.value;
+
+    if (mail === '' || password === '') {
+        window.alert('все поля должны быть заполнены');
+    }
+
+    const formData = {
+        mail,
+        password,
+    }
+    console.log(formData);
+
+    form.reset();
+};
 
