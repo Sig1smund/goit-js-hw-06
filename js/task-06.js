d56c5c9e5f012ad874987d66a85550633e3ba785
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2021-11-09 17:20:14 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,5 +3,5 @@ const inputField = document.getElementById('validation-input');
 inputField.addEventListener('blur', (event) => {
     const almightyArray = [...event.currentTarget.value];
     console.log(almightyArray);
-    almightyArray.length === 6 ? inputField.style.borderColor = '#4caf50' : inputField.style.borderColor = '#f44336';
+    almightyArray.length === Number(inputField.dataset.length) ? inputField.style.borderColor = '#4caf50' : inputField.style.borderColor = '#f44336';
 });
