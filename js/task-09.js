64ba005cdd53e8c4316839c21d22bc5e74c8e70d
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2021-11-09 22:20:35 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const triggerBtn = document.querySelector('.change-color')
+const spanColor = document.querySelector('.color')
+const body = document.querySelector('body')
+
+triggerBtn.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = body.style.backgroundColor;
+});
+
+  
+  
