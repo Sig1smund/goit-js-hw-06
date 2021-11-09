1e73ed28056cd54844af84c61e4ca498e7cc4fd1
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2021-11-09 15:40:12 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -4,7 +4,6 @@ const decrement = document.querySelector('button[data-action="decrement"]');
 const increment = document.querySelector('button[data-action="increment"]');
 const counter = document.getElementById('value');
 
-
 const onIncrementButtonClick = () => {
     counterValue += 1;
     counter.textContent = counterValue;
