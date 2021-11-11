20b3efbba7688cd31ccdeb033dd2d414f32baa71
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2021-11-11 10:41:00 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,8 +3,10 @@ const inputField = document.getElementById('validation-input');
 inputField.addEventListener('blur', (event) => {
     const almightyArray = [...event.currentTarget.value];
     if (almightyArray.length === Number(inputField.dataset.length)) {
+        inputField.classList.remove('invalid')
         inputField.classList.add('valid')
     } else {
         inputField.classList.add('invalid')
+        inputField.classList.remove('valid')
     }
 });
